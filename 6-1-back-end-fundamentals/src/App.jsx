94be2d844079d4355ea2e3cf598653da708b07a1
janadamdam <s202323190@kfupm.edu.sa>
2026-03-30 13:27:49 +0300

    TODO8 complete

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -369,6 +369,12 @@ export default function App() {
   //   .then((data) => {
   //     setStudent(data);
   //   });
+  fetch('http://localhost:3000/student')
+    .then((res) => res.json())
+    .then((data) => {
+      setStudent(data);
+    });
+    
 
   return (
     <main className="app-shell">
